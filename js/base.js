658f56100c9e8482501997f26955312c14e271b2
Lucas Braga Ferreira <lucasbrafer@gmail.com>
2021-10-09 14:44:29 -0300

    page gas first layout

--- js/base.js
+++ js/base.js
@@ -22,3 +22,30 @@ onload = () => {
 
   mostra();
 };
+
+function formatCurrency(val, name) {
+  let numVal = Number(val)
+  if(val && !Number.isNaN(val)) {
+    let result = numVal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    document.getElementById(name).value = result
+  }
+}
+
+function numberToReal(name) {
+  var elemento = document.getElementById(name);
+  var valor = (elemento.value).split(' ')[1];
+  
+
+  valor = valor + '';
+  valor = parseInt(valor.replace(/[\D]+/g, ''));
+  valor = valor + '';
+  valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+  if (valor.length > 6) {
+      valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+  }
+
+  elemento.value = 'R$ ' + valor;
+  if(valor == 'NaN') elemento.value = 'R$ ' + '';
+  
+}
